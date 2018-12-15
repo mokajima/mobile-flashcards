37c56dd8b210031548c7cb169df29f4bff05f214
mokajima <mokajima.85z@gmail.com>
2018-12-15 18:24:49 +0900

    feat: Add a TextButton component

---
+++ components/TextButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+function TextButton({ children, onPress, buttonStyle = {}, labelStyle = {} }) {
+  return (
+    <TouchableOpacity onPress={onPress} style={[styles.button, buttonStyle]}>
+      <Text style={[styles.label, labelStyle]}>{children}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    borderRadius: 4,
+    paddingVertical: 15,
+    paddingHorizontal: 10
+  },
+  label: {
+    fontSize: 16
+  }
+})
+
+export default TextButton
